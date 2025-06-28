52f65b801ee62588875891f24dd3e32902820a65
Sumedh Charjan <sumedhcharjan321@gmail.com>
2025-06-28 23:50:10 +0530

    added courses page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage'; 
 import HomePage from './pages/HomePage';
-// import CoursePage from './pages/CoursePage';
+import CoursesPage from './pages/CoursesPage';
 import ArticlesPage from './pages/ArticlesPage';
 // import FinToolsPage from './pages/FinToolsPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import Course from './pages/CoursePage';
 
 function App() {
   return (
@@ -15,11 +16,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} /> 
-        {/* <Route path="/courses" element={<CoursePage />} /> */}
+        <Route path="/courses" element={<CoursesPage />} />
         <Route path="/articles" element={<ArticlesPage />} />
            {/* <Route path="/fintools" element={<FinToolsPage />} />   */}
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/course" element={<Course />} />
       </Routes>
     </Router>
   );
