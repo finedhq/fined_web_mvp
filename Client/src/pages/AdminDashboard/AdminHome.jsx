cbc93a1f06139128e46a93232c7f763915f36d72
kalpm1110 <kalpm1832@gmail.com>
2025-07-12 00:11:59 +0530

    HomePage Responsiveness!

--- Client/src/pages/AdminDashboard/AdminHome.jsx
+++ Client/src/pages/AdminDashboard/AdminHome.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const AdminHome = () => {
   const navigate = useNavigate();
-  const { user } = useAuth0();
+  const { user,isLoading,isAuthenticated } = useAuth0();
+  const [role, setrole] = useState("")
+useEffect(() => {
+  if(!isLoading && isAuthenticated){
+    const roles = user?.["https://fined.com/roles"];
+    setrole(roles)
+    if(role!=="Admin") navigate('/');
+  }else if(!isLoading && !isAuthenticated) navigate('/')
+
+}, [user,isAuthenticated,isLoading])
+
 
   const cards = [
     {
