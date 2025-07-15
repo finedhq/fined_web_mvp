321b452991328cd440d01eb99ee3ec39fa5239e0
Aditya-alpha <bandraladitya5544@gmail.com>
2025-07-15 11:29:05 +0530

    deploying

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance=axios.create({
-    baseURL:'https://fined.onrender.com/api',withCredentials:true
+    baseURL:'http://localhost:8000/api',withCredentials:true
 })
 export default instance;
