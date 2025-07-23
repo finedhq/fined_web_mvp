dd4c43f20e1c8c1c16267f88bd979c5112578aef
Aditya-alpha <bandraladitya5544@gmail.com>
2025-07-23 14:16:43 +0530

    deploying

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance=axios.create({
     // baseURL:'https://finedwebmvp-production.up.railway.app/api',withCredentials:true
-    // baseURL:'https://finedwebmvp-production.up.railway.app/api',withCredentials:true
-        baseURL:'https://srv921874.hstgr.cloud/api',withCredentials:true
+    baseURL:'https://finedwebmvp-production.up.railway.app/api',withCredentials:true
+        // baseURL:'https://srv921874.hstgr.cloud/api',withCredentials:true
 })
 export default instance;
