6032e36e45c1a6b2eed0149133cbf8bf8d6e8bf3
Aditya-alpha <bandraladitya5544@gmail.com>
2025-07-27 18:55:24 +0530

    deployed on hostinger

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance=axios.create({
-    // baseURL:'https://finedwebmvp-production.up.railway.app/api',withCredentials:true
-        baseURL:'https://api.myfined.com/api',withCredentials:true
+    baseURL:'https://finedwebmvp-production.up.railway.app/api',withCredentials:true
+        // baseURL:'https://api.myfined.com/api',withCredentials:true
 })
 export default instance;
