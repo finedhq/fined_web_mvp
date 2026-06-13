c3831e67e9a35ccc68fe6480e03e4cd531d07393
Shravan Mutha <shravanmutha45@gmail.com>
2026-06-13 22:14:42 +0530

    url changes

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://my-fined.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
